fd56cec09ab01ee63033bf5673d0eed1cf5cbe45
Kraussie <nathanialkrauss@gmail.com>
2020-04-20 18:47:08 -0400

    updated UI, still needs work

--- app.py
+++ app.py
@@ -14,41 +14,34 @@ def index():
 def test():
     school = request.args['sch']
     if school == 'gen':
-        txtColor = '#ccc'
-        bknd = '#fff'
+        bknd = '#0D47A1'
         hlight = '#f9a825'
-        headIMG = 'https://static.wixstatic.com/media/54af3f_ef2e9753337741b7ba8f5f8507255acc~mv2.png/v1/fill/w_320,h_254,al_c,q_85,usm_0.66_1.00_0.01/54af3f_ef2e9753337741b7ba8f5f8507255acc~mv2.webp'
+        headIMG = 'https://images.sftcdn.net/images/t_app-cover-l,f_auto/p/ce2ece60-9b32-11e6-95ab-00163ed833e7/260663710/the-test-fun-for-friends-screenshot.jpg'
     elif school == 'crh':
-        txtColor = '#fff'
         bknd = '#163052'
         hlight = '#7DA7D9'
         headIMG = 'https://www-tc.pbs.org/wgbh/americanexperience/media/filer_public_thumbnails/filer_public/d1/46/d1464710-a056-41ba-a15b-0f2fc5bfa34a/jfk_harvard_800.jpg__800x529_q85_crop_subsampling-2_upscale.jpg'
     elif school == 'da':
-        txtColor = '#fff'
         bknd = '#1b5e20'
         hlight = '#00e676'
         headIMG = 'https://d13b2ieg84qqce.cloudfront.net/705b9a8896af4dad10ba668e7b9f8a9363189480.jpg'
     elif school == 'hkiss':
-        txtColor = '#fff'
         bknd = '#0D47A1'
         hlight = '#eee'
         headIMG = 'https://www.hotchkiss.org/uploaded/images/Alumni/Alumni_Accomplishments/Banner_Janney77.jpg'
     elif school == 'lville':
-        txtColor = '#fff'
         bknd = '#000'
         hlight = '#D50000'
         headIMG = 'https://prabook.com/web/show-photo-icon.jpg?id=13355&width=220&cache=false'
     elif school == 'and':
-        txtColor = '#fff'
         bknd = '#0D4741'
         hlight = '#eee'
         headIMG = 'https://i.ytimg.com/vi/OJTq4j2kDdw/maxresdefault.jpg'
     elif school == 'ext':
-        txtColor = '#fff'
         bknd = '#000'
         hlight = '#D50000'
         headIMG = 'https://media.nbcbayarea.com/2019/09/Yang_Thumb.jpg?resize=850%2C478'
-    return render_template('test.html', txtColor=txtColor, school=school, bknd=bknd, hlight=hlight, headIMG=headIMG)
+    return render_template('test.html', school=school, bknd=bknd, hlight=hlight, headIMG=headIMG)
 
 @app.route('/stats')
 def stats():
