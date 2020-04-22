b693d7df543861ae6d57afa3adbf63114afab05d
Kraussie <nathanialkrauss@gmail.com>
2020-04-22 18:19:18 -0400

    new log spacing

--- utils.py
+++ utils.py
@@ -16,7 +16,7 @@ DEFAULT_LOGGING = {
     'version': 1,
     'formatters': {
         'standard': {
-            'format': color.RED + '%(asctime)-2s' + color.END + color.PURPLE + ' %(levelname)-8s' + color.END + '%(message)s',
+            'format': color.RED + '%(asctime)-4s' + color.END + color.PURPLE + ' %(levelname)-8s' + color.END + '%(message)s',
             'datefmt': '%Y-%m-%d | %H:%M:%S' },
     },
     'handlers': {
