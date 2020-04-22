d14d222a4176904ff7a3b309dac575cce28d654d
Kraussie <nathanialkrauss@gmail.com>
2020-04-22 18:15:56 -0400

    logging, removed MS and changed spacing

--- utils.py
+++ utils.py
@@ -16,7 +16,7 @@ DEFAULT_LOGGING = {
     'version': 1,
     'formatters': {
         'standard': {
-            'format': color.RED + '%(asctime)s,%(msecs)d' + color.END + color.PURPLE + ' %(levelname)s' + color.END + '%(message)s',
+            'format': color.RED + '%(asctime)d' + color.END + color.PURPLE + ' %(levelname)d' + color.END + '%(message)s',
             'datefmt': '%Y-%m-%d:%H:%M:%S' },
     },
     'handlers': {
