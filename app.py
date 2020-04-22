3fc83315c94a6aa85d4f25d5069613fd0ce8f94e
Kraussie <nathanialkrauss@gmail.com>
2020-04-22 15:29:52 -0400

    reverted to initial logging code

--- app.py
+++ app.py
@@ -1,18 +1,11 @@
 import dataset, logging
 from flask import Flask, render_template, request, url_for
 
-# create logger
-logger = logging.getLogger("/var/log/app.log")
-logger.setLevel(logging.DEBUG)
-# create console handler and set level to debug
-ch = logging.StreamHandler()
-ch.setLevel(logging.DEBUG)
-# create formatter
-formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
-# add formatter to ch
-ch.setFormatter(formatter)
-# add ch to logger
-logger.addHandler(ch)
+#create logger
+LOG_FILENAME = '/var/log/app.log'
+logging.basicConfig(filename=LOG_FILENAME,level=logging.DEBUG)
+
+logging.debug('This message should go to the log file')
 
 # "application" code
 logger.debug("debug message")
@@ -21,20 +14,18 @@ logger.warn("warn message")
 logger.error("error message")
 logger.critical("critical message")
 
+#database initialization
 db = dataset.connect('sqlite:///index.db')
 table = db['resp_table']
-logging.debug('DATABASE INTITIALIZED')
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    logger.info('RENDERING >> index.html')
     return render_template('index.html')
 
 @app.route('/test')
 def test():
-    logging.critical('RENDERING >> test.html')
     school = request.args['sch']
     if school == 'gen':
         bknd = '#163052'
